fd98587da16ef9a50c31e5c1da939ce23d21caf9
Diego Linhares <diegomlinhares@gmail.com>
2020-04-09 19:35:43 -0300

    Equilibrium Problem

---
+++ complexity_time/equlibrium.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Complexity O(N)
+class EquilibriumProblem
+  def self.solution(a)
+    left_elements_sum = a[0]
+    right_elements_sum = a[1, a.length - 1].sum
+    difference = (left_elements_sum - right_elements_sum).abs
+
+    (1..a.length - 2).each do |i|
+      left_elements_sum += a[i]
+      right_elements_sum -= a[i]
+      current_difference = (left_elements_sum - right_elements_sum).abs
+
+      difference = current_difference if difference > current_difference
+    end
+    difference
+  end
+end
+
+puts EquilibriumProblem.solution([3, 1, 2, 4, 3])
