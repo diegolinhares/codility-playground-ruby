1dfbb3f2a7c2c0d47ddff44670d53f205e5d7dd4
Diego Linhares <diegomlinhares@gmail.com>
2020-04-10 14:54:32 -0300

    My Solution

---
+++ counting_elements/max_counters.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class MaxCounters
+  def self.solution(n, a)
+    counters = Array.new(n, 0)
+
+    a.each do |instruction|
+      if instruction > n
+        max_counter = counters.max
+        counters.map! { max_counter }
+      else
+        counters[instruction - 1] += 1
+      end
+    end
+
+    counters
+  end
+end
+
+puts MaxCounters.solution(5, [3, 4, 4, 6, 1, 4, 4])
